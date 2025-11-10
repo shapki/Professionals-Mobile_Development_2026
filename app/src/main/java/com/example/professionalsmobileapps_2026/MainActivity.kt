ef18c4537972e7f802c9d367a47e5b8701a78e4a
Игорь Шапкин <shapkin.igorek@gmail.com>
2025-11-10 10:22:14 +0300

    App Display Changes + Sign-In/Auth Progress

--- app/src/main/java/com/example/professionalsmobileapps_2026/MainActivity.kt
+++ app/src/main/java/com/example/professionalsmobileapps_2026/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.professionalsmobileapps_2026
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        val window = this.window
+        WindowCompat.getInsetsController(window, window.decorView)?.apply {
+            isAppearanceLightStatusBars = false
+            isAppearanceLightNavigationBars = false
+        }
+
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_main)
     }
 }
